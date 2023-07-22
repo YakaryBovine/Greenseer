5223f773014035f30b30f3ecf4a35b9078532d23
Zakary Boven <zakaryboven@gmail.com>
2023-07-23 00:30:19 +1200

    Added a way to list all goals

---
+++ Greenseer/Modules/UserCommands.cs
@@ -0,0 +1,23 @@
+﻿using Discord.Interactions;
+using Greenseer.Models;
+using Greenseer.Services;
+
+namespace Greenseer.Modules;
+
+public sealed class UserCommands : InteractionModuleBase<SocketInteractionContext>
+{
+  private readonly IMongoDBService _mongoDbService;
+
+  public UserCommands(IMongoDBService mongoDbService)
+  {
+    _mongoDbService = mongoDbService;
+  }
+  
+  [SlashCommand("listgoals", "Lists all of the Goals in the game.")]
+  public async Task ListGoals()
+  {
+    var listOfGoals = await _mongoDbService.GetAsync();
+    var readableListOfGoals = string.Join(Environment.NewLine, listOfGoals.Select(x => x.Name));
+    await RespondAsync($"**Goals** {Environment.NewLine}{readableListOfGoals}");
+  }
+}
