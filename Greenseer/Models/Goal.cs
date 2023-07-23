887ec50924e69e02de848e782bcfb88a415397bf
Zakary Boven <zakaryboven@gmail.com>
2023-07-23 13:03:34 +1200

    Made Goals use Name for their unique ID

--- Greenseer/Models/Goal.cs
+++ Greenseer/Models/Goal.cs
@@ -1,14 +1,10 @@
-﻿using MongoDB.Bson;
-using MongoDB.Bson.Serialization.Attributes;
+﻿using MongoDB.Bson.Serialization.Attributes;
 
 namespace Greenseer.Models;
 
 public sealed class Goal
 {
   [BsonId]
-  [BsonRepresentation(BsonType.ObjectId)]
-  public string? Id { get; set; }
-
   public string Name { get; set; } = null!;
   
   /// <summary>What the user needs to do to complete the goal.</summary>
